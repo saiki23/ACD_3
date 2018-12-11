5322cd999ea2eed0fa4e197ae6a0ab3722f14caa
saiki23 <saiki2144@gmail.com>
2018-12-11 09:41:28 +0530

    Update Assignment3_1.r

--- Assignment3_1.r
+++ Assignment3_1.r
@@ -1,12 +1,3 @@
-#creating a matrix
-#step1: Go through the for loop, 
-#step2: Ask the matrix to value to be filled
-#step3: ensure that if the index[row]=index[col], skip
-#step4: also ensure that if value of col > value of row, skip
-#step5: pront the new matrix
-#step6: print the number of non-zero elements in the matrix
-#step6.1: use a function that returns the desired number
-
 
 zeromat<- matrix(0:0, nrow= 4, ncol=4)
 zeromat
